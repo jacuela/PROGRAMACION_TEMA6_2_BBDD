7cc65a6d196f6971b1f2a8a99bb558a8581630fd
Juan Antonio Cuello Alarcon <jcuelloa@uoc.edu>
2023-04-20 10:51:30 +0200

    Ejercicio 1

---
+++ src/ejercicio1/BD_World.java
@@ -0,0 +1,259 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio1;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author jacuela
+ */
+public class BD_World {
+
+    
+    private static final String URL = "jdbc:mariadb://localhost:3306/world";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "";
+    private static Connection conexion;
+    
+    public BD_World(){
+         //this.conectar();
+        
+    }
+    
+    
+    public static Connection conectar(){
+        
+            //***********************************************************
+            //    CARGAMOS EL DRIVER
+            //***********************************************************
+            try {
+            
+                Class.forName("org.mariadb.jdbc.Driver").newInstance();
+                //System.out.println("Driver <org.mariadb.jdbc.Driver> cargado.");
+            
+            } catch (Exception ex) {
+                System.out.println("Error, no se ha podido cargar MariaDB JDBC Driver");
+            }
+        
+            
+            try {
+                //***********************************************************
+                //    NOS CONECTAMOS A LA BASE DE DATOS
+                //***********************************************************
+                
+                conexion = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+                return conexion;
+                
+                
+                
+            } catch (SQLException ex) {
+                //System.out.println(ex.getMessage());
+                return null;
+            }
+        
+    }
+    
+    
+    public static void cerrarConexion() {
+            try {
+                conexion.close();
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+            
+        }
+    
+    
+    //Metodo para consultar los habitantes que tiene un pais dado
+    public static void habitantesPais(String nombrePais) {
+        
+        try {
+            
+            Statement statement = conexion.createStatement();
+          
+            ResultSet rs = statement.executeQuery("SELECT name,population FROM country WHERE"
+                    + " name='"+nombrePais+"'");
+           
+            while (rs.next()) {
+
+                    String nombrepais = rs.getString("name");
+                    String habitantes = rs.getString("population");
+                    
+                    System.out.printf("\nPAIS             HABITANTES\n");
+                    System.out.printf("---------------------------\n");
+                    System.out.printf("%-20s %s\n",nombrepais, habitantes);
+                   
+            }
+
+            rs.close();
+            statement.close();
+            //conexion.close();    
+        
+        } catch (SQLException ex) {
+            Logger.getLogger(main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+
+    //Metodo para consultar los habitantes que tiene una coidad. Se incluye el
+    //nombre del paius en la salida
+    public static void habitantesCiudad(String nombreCiudad) {
+        try {
+            
+            Statement statement = conexion.createStatement();
+          
+            ResultSet rs = statement.executeQuery("SELECT city.name,country.name,city.population"
+                    + " FROM city INNER JOIN country ON city.countrycode=country.code "
+                    + "WHERE city.name='"+nombreCiudad+"'");
+           
+            while (rs.next()) {
+
+                    String ciudad = rs.getString("city.name");
+                    String pais = rs.getString("country.name");
+                    String habitantes = rs.getString("city.population");
+                    
+                    System.out.printf("\nCIUDAD           PAIS           HABITANTES\n");
+                    System.out.printf("---------------------------------------------\n");
+                    System.out.printf("%-20s %-20s %s\n",ciudad,pais,habitantes);
+                   
+            }
+
+            rs.close();
+            statement.close();
+            //conexion.close();    
+        
+        } catch (SQLException ex) {
+            Logger.getLogger(main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+
+    
+    
+    public static void paisesMinHabitantes(int minhabit) {
+        try {
+            
+            Statement statement = conexion.createStatement();
+          
+            ResultSet rs = statement.executeQuery("SELECT name,population FROM country WHERE"
+                    + " population>'"+minhabit+"'");
+           
+            
+            
+            System.out.printf("\nPAIS             HABITANTES\n");
+            System.out.printf("---------------------------\n");
+                    
+            while (rs.next()) {
+
+                    String nombrepais = rs.getString("name");
+                    String habitantes = rs.getString("population");
+                    
+                    System.out.printf("%-20s %s\n",nombrepais, habitantes);
+                   
+            }
+
+            rs.close();
+            statement.close();
+            //conexion.close();    
+        
+        } catch (SQLException ex) {
+            Logger.getLogger(main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    
+    
+    }
+
+    public static void ciudadesMinHabitantes(int minhabit) {
+       try {
+            
+            Statement statement = conexion.createStatement();
+          
+           
+             ResultSet rs = statement.executeQuery("SELECT city.name,country.name,city.population"
+                    + " FROM city,country "
+                    + "WHERE country.code=city.countrycode AND city.population>'"+minhabit+"' ORDER BY 3 DESC");
+             
+             
+             System.out.printf("\nCIUDAD           PAIS           HABITANTES\n");
+             System.out.printf("---------------------------------------------\n");
+                    
+            while (rs.next()) {
+
+                String ciudad = rs.getString("city.name");
+                String pais = rs.getString("country.name");
+                String habitantes = rs.getString("city.population");
+                    
+                System.out.printf("%-20s %-20s %s\n",ciudad,pais,habitantes); 
+            }
+
+            rs.close();
+            statement.close();
+            //conexion.close();    
+        
+        } catch (SQLException ex) {
+            Logger.getLogger(main.class.getName()).log(Level.SEVERE, null, ex);
+        } 
+        
+    
+    
+    }
+    
+    //Metodo para listar los habitantes de todas las ciudades de la
+    //base de datos. Mostramos tb el nombre del pais.
+    //La variable limite es para limitar la salida
+    public static void mostrarHabitantesCiudad(int limite) {
+        try {
+            String sql;
+            Statement statement = conexion.createStatement();
+            
+            if (limite!=0){
+                sql=String.format("SELECT city.name,country.name,city.population"
+                    + " FROM city INNER JOIN country ON city.countrycode=country.code "
+                    + "ORDER BY 3 DESC LIMIT %d",limite);
+            }
+            else{
+                sql=String.format("SELECT city.name,country.name,city.population"
+                    + " FROM city INNER JOIN country ON city.countrycode=country.code "
+                    + "ORDER BY 3 DESC LIMIT 50");
+            }
+            
+            ResultSet rs = statement.executeQuery(sql);
+           
+            System.out.printf("\nCIUDAD           PAIS           HABITANTES\n");
+            System.out.printf("---------------------------------------------\n");
+            while (rs.next()) {
+
+                    String ciudad = rs.getString("city.name");
+                    String pais = rs.getString("country.name");
+                    String habitantes = rs.getString("city.population");
+                    
+                    System.out.printf("%-20s %-20s %s\n",ciudad,pais,habitantes);
+                   
+            }
+
+            rs.close();
+            statement.close();
+            //conexion.close();    
+        
+        } catch (SQLException ex) {
+            Logger.getLogger(main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+    
+    
+    
+    
+    
+}
+
---
+++ src/ejercicio1/main.java
@@ -0,0 +1,100 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio1;
+
+import java.sql.*;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JFileChooser;
+
+
+/**
+ *
+ * @author jacuela
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner entrada = new Scanner(System.in);
+        
+        //BD_World bdMundo=new BD_World();
+        System.out.print("Conectando con base de datos WORLD......");
+        if (BD_World.conectar()==null) 
+            System.out.println("\033[31mERROR");
+            
+        else{
+            System.out.println("\033[32mOK");
+        
+            boolean salir = false;
+            int opcion; //Guardaremos la opcion del usuario
+        
+            
+            while(!salir){
+           
+                System.out.println("\n\n\033[34m***** BBDD WORLD *****\033[30m");
+                System.out.println("\033[34m***************************\033[30m");
+
+                System.out.println("1. Habitantes de paises");
+                System.out.println("2. Habitantes de ciudades");
+                System.out.println("3. Paises con un minimo de X habitantes");
+                System.out.println("4. Ciudades con un minimo de X habitantes");
+                System.out.println("5. Habitantes de todas las ciudades");
+                System.out.println("6. Salir");
+
+                System.out.print("Elige una opcion >");
+                opcion = Integer.parseInt(entrada.nextLine());
+
+                switch(opcion){
+                    case 1:
+                         System.out.print("Nombre del pais:");
+                         String nombrePais = entrada.nextLine();
+                         BD_World.habitantesPais(nombrePais);
+                         break;
+
+                    case 2:
+                         System.out.print("Nombre de la ciudad:");
+                         String nombreCiudad = entrada.nextLine();
+                         BD_World.habitantesCiudad(nombreCiudad);
+                         break;
+
+                    case 3:
+                         System.out.print("Número min. de habitantes del pais:");
+                         int habitantes = Integer.parseInt(entrada.nextLine());
+                         BD_World.paisesMinHabitantes(habitantes);
+                         break;
+
+                    case 4:
+                         System.out.print("Número min. de habitantes de la ciudad:");
+                         int habitantes2 = Integer.parseInt(entrada.nextLine());
+                         BD_World.ciudadesMinHabitantes(habitantes2);
+                         break;
+
+                    case 5:
+                         System.out.print("Indica num filas a mostrar [0 para todas]: ");
+                         int limite = Integer.parseInt(entrada.nextLine());
+                         BD_World.mostrarHabitantesCiudad(limite);
+                         break;     
+                         
+                    case 6:
+                        salir=true;
+                        BD_World.cerrarConexion();
+                        break;
+
+                     default:
+                        System.out.println("Solo números entre 1 y 5");
+                }
+            
+            } //fin menu
+        
+        } //fin else    
+    }//fin main 
+    
+}
